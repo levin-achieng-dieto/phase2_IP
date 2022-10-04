32b9adc55c03fd1539f944dab7ceff2f61f1acf5
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-04 13:21:31 +0300

    formating logo component

--- src/component/Logo.js
+++ src/component/Logo.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 
 function Logo(){
     return (
-        <div>
-            <h1>The Royal Ways</h1>
-            <h2>DESTINATIONS</h2>
-            <button>SIGN IN</button>
+        <div className="flex justify-between text-white bg-black h-32 font-bold text-2xl pl-20">
+            <h1 className="pt-12 pl-8">The Royal Ways</h1>
+            <div className="flex">
+            <h2 className="pr-10 pt-12">DESTINATIONS</h2>
+            <Link to="/Form" className="pr-20 pt-12">SIGN IN</Link>
+            </div>
         </div>
     )
 }
