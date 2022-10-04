05845ad17220b64837a8e637eaa984fba5a5330c
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-04 13:23:03 +0300

    changing location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from "./component/NavBar"
+import Form from "./component/Form"
+import SoloTravel from "./component/SoloTravel"
+import FamilyTravel from "./component/FamilyTravel"
+import GroupTravel from "./component/GroupTravel"
+import Home from "./component/Home"
+//import Logo from "./component/Logo"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Form" element={<Form />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/SoloTravel" element={<SoloTravel />} />
+        <Route path="/FamilyTravel" element={<FamilyTravel />} />
+        <Route path="/GroupTravel" element={<GroupTravel />} />
+      </Routes>
+    </>
   );
 }
 
--- src/component/LowerNav.js
+++ src/component/LowerNav.js
@@ -3,8 +3,13 @@ import React from "react";
 
 function LowerNav(){
     return (
-        <div>
-            LowerNav
+        <div className=" pl-20 bg-black text-white h-32 mt-1">
+            <div className="flex justify-between w-2/4 pt-11 text-2xl font-bold">
+                <h2 className="pl-8">SoloTravel</h2>
+                <h2>FamilyTravel</h2>
+                <h2>GroupTravel</h2>
+                <h2>HOTLIST</h2>
+            </div>
         </div>
     )
 }
