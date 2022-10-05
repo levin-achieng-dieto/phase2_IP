d213b8f56019240ce611259864533efcd8136004
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-05 20:01:51 +0300

    correcting spelling

--- src/component/Home.js
+++ src/component/Home.js
@@ -7,7 +7,7 @@ function Home(){
             <NavBar />
             <div className="text-center">
                 <div className="w-full">
-                    <img src="https://i.postimg.cc/d1q9JQWN/The-Chandi-Boutique-Resort-Spa-Mataram-Hotels-Combined.jpg" alt="HomePage" className="object-fill w-full h-96" />
+                    <img src="https://i.postimg.cc/d1q9JQWN/The-Chandi-Boutique-Resort-Spa-Mataram-Hotels-Combined.jpg" alt="HomePage" className="w-full h-96" />
                 </div>
                 <h2 className="pt-10 pb-5 text-3xl  font-bold">HOLLIDAYCOTTAGES</h2>
                 <p className="text-2xl font-light">Whether you're looking for an Airbnb alternative <br />in the UK that's by the sea, nestled in the country or <br />set bang in the middle of a city, <br />you'll want to keep scrolling for our selection of self-catering providers.</p>
