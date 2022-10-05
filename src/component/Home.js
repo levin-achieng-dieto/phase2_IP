b6e83370dfe064dde43cb0159786ae7bbc54e7c2
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-05 08:29:38 +0300

    nesting other components  in home

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,11 +5,14 @@ function Home(){
     return (
         <>
             <NavBar />
-            <div className="h-4/6">
-                <img src="https://i.postimg.cc/Pq2bqQs1/matt-drenth-Bkw-a-S2-Nvg8-unsplash.jpg" alt="HomePage" className="" />
-                <h2>location</h2>
-                <p>some Long text</p>
-                <h1>Auther</h1>
+            <div className="text-center">
+                <div className="w-full">
+                    <img src="https://i.postimg.cc/d1q9JQWN/The-Chandi-Boutique-Resort-Spa-Mataram-Hotels-Combined.jpg" alt="HomePage" className="object-fill w-full h-96" />
+                </div>
+                <h2 className="pt-10 pb-5 text-3xl  font-bold">HOLLIDAYCOTTAGES</h2>
+                <p className="text-2xl font-light">Whether you're looking for an Airbnb alternative <br />in the UK that's by the sea, nestled in the country or <br />set bang in the middle of a city, <br />you'll want to keep scrolling for our selection of self-catering providers.</p>
+                <h1 className="pt-5 mb-20">@ROSHINA JOWAHEER</h1>
+                <div className="h-48 bg-black"/>
             </div>
         </>
     )
