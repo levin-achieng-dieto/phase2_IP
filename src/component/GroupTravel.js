d47eacf3eae1aab6b29dadd68e0981ddc57a6e5a
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-06 07:30:08 +0300

    fetching group destinations

--- src/component/GroupTravel.js
+++ src/component/GroupTravel.js
@@ -1,14 +1,35 @@
 import React from "react";
 import Logo from "./Logo";
 import Destinations from "./Destinations";
+import Card from "./Card"
 
 
 function GroupTravel(){
+
+    const [groups, setGroups] = React.useState([])
+
+    function groupDes(){
+        fetch("http://localhost:3000/groupTravel")
+        .then((response) => response.json())
+        .then((resp) => setGroups(resp))
+    }
+
+    React.useEffect(() => {
+        groupDes()
+    }, [])
+
+    const groupDestination = groups.map((group) => {
+        return <Card key={group.id} image={group.image}
+        location={group.location} Destination={group.Destination}
+        auther={group.auther}/>
+    })
+
     return (
         <div>
             <Logo />
             <Destinations />
             GroupTravel
+            {groupDestination}
         </div>
     )
 }
