23d8c0ffe3e930909205cc7d57b6328723760014
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-06 07:31:04 +0300

    fetching family destinations

--- src/component/FamilyTravel.js
+++ src/component/FamilyTravel.js
@@ -1,14 +1,35 @@
 import React from "react";
 import Logo from "./Logo";
 import Destinations from "./Destinations";
+import Card from "./Card"
 
 
 function FamilyTravel(){
+
+    const [family, setfamily] = React.useState([])
+
+    function familyDestination(){
+        fetch("http://localhost:3000/familyTravel")
+        .then((response) => response.json())
+        .then((resp) => setfamily(resp))
+    }
+
+    React.useEffect(() => {
+        familyDestination()
+    }, [])
+
+    const familyDes = family.map((destination) => {
+        return <Card key={destination.id} image={destination.image}
+        location={destination.location} Description={destination.Description}
+        auther={destination.auther} />
+    })
+
     return (
         <div>
             <Logo />
             <Destinations />
             Family Travel
+            {familyDes}
         </div>
     )
 }
