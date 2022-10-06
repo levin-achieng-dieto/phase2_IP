ee02450c649142aecc95b0f4b086ea7785dac030
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-06 07:30:44 +0300

    fetching solo destinations

--- src/component/HotList.js
+++ src/component/HotList.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Logo from "./Logo";
 import Destinations from "./Destinations";
+//import SoloTravel from "./SoloTravel";
+//import Card from "./Card"
 
 function HotList(){
+
+    
+
 return (
     <>
         <Logo />
         <Destinations />
+        {/* <SoloTravel result={result}/> */}
+        {/* {result} */}
         HOTLIST
     </>
 )
